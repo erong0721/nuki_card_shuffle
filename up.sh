b5f0805671b49d97410f4a0559aba124f0902c26
erong.0721 <erong.0721@gmail.com>
2023-03-25 23:34:19 +0900

    up

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 cp css/nuki_card_shuffle.min.css s3://adultkeikaku.cdn/css
-aws s3 cp js/nuki_card_shuffle.min.js s3://adultkeikaku.cdn/js
+aws s3 cp css/nuki_card_shuffle.min.css s3://adultkeikaku.cdn/css/
+aws s3 cp js/nuki_card_shuffle.min.js s3://adultkeikaku.cdn/js/
